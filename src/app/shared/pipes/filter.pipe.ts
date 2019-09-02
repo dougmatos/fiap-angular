26bdacdd4f10af7fd308606f3c839d222c622a30
dougmatos <dougmooca@gmail.com>
2019-09-01 21:49:36 -0300

    ajustes nos pipes

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -1,16 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-
 @Pipe({name: 'filter'})
-export class FilterPipe implements PipeTransform{
-    transform(value: any, filterBy: String) {
-        return filterBy == undefined ? value 
-            : value.filter(x => this.normaliza(x.name).indexOf(this.normaliza(filterBy)) >= 0);
+export class FilterPipe implements PipeTransform {
+    transform(value: any, filterBy: string) {
+        return filterBy === undefined ? value
+            : value.filter((x: { name: any; }) => this.normaliza(x.name).indexOf(this.normaliza(filterBy)) >= 0);
     }
 
     normaliza(arg: any): string {
         arg = arg.toLowerCase().trim();
-        arg = arg.replace(/[ãâáà]/gim, "a");
+        arg = arg.replace(/[ãâáà]/gim, 'a');
         return arg;
     }
 }
--- src/app/shared/pipes/order.pipe.ts
+++ src/app/shared/pipes/order.pipe.ts
@@ -1,13 +1,10 @@
-import { Pipe, PipeTransform } from '@angular/core'
-import * as _  from "lodash";
-
+import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
 
 @Pipe({name: 'order'})
-export class OrderPipe implements PipeTransform{
-    
-    
+export class OrderPipe implements PipeTransform {
     transform(value: any, direction: any) {
-        if(direction == undefined) return value;
-        return _.orderBy(value, ["name"], [direction]);
+        if (direction === undefined) { return value; }
+        return _.orderBy(value, ['name'], [direction]);
     }
 }
